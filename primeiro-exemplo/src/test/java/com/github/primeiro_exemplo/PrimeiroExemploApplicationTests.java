4596b9c7e9c01ab70d38d82010c90828089e3468
David Nicolini <98845165+davidnicolini@users.noreply.github.com>
2026-06-09 16:24:14 -0300

    Remove comment from ActiveProfiles annotation
    Signed-off-by: David Nicolini <98845165+davidnicolini@users.noreply.github.com>

--- primeiro-exemplo/src/test/java/com/github/primeiro_exemplo/PrimeiroExemploApplicationTests.java
+++ primeiro-exemplo/src/test/java/com/github/primeiro_exemplo/PrimeiroExemploApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test") // <-- Força o Spring a usar as configurações de teste
+@ActiveProfiles("test") 
 class PrimeiroExemploApplicationTests {
 
     @Test
