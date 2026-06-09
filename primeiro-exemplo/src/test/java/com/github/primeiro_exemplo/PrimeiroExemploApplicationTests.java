04819ba4c12738a5a2f40f1391517b9725713b92
David Nicolini <98845165+davidnicolini@users.noreply.github.com>
2026-06-09 16:22:49 -0300

    Add ActiveProfiles annotation for test configurations
    Added ActiveProfiles annotation to use test configurations.
    
    Signed-off-by: David Nicolini <98845165+davidnicolini@users.noreply.github.com>

--- primeiro-exemplo/src/test/java/com/github/primeiro_exemplo/PrimeiroExemploApplicationTests.java
+++ primeiro-exemplo/src/test/java/com/github/primeiro_exemplo/PrimeiroExemploApplicationTests.java
@@ -2,12 +2,14 @@ package com.github.primeiro_exemplo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // <-- Força o Spring a usar as configurações de teste
 class PrimeiroExemploApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
